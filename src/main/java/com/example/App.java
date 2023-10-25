4ce055710fd1b0e3ea85b09bcfe6a904e3e76cef
lai2-24 <lai2-24@itismeucci.com>
2023-10-25 12:32:40 +0200

    added multithreading

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -9,8 +12,11 @@ public class App
     public static void main( String[] args )
     {
         try {
-            Server server = new Server(6666);
-            server.start();
+            ServerSocket serverSocket = new ServerSocket(6666);
+            do {
+                Server s = new Server(serverSocket.accept()); 
+                s.start();
+            } while (true);
         } catch (Exception e) {
             System.err.println(e.getMessage());
             System.exit(1);
--- src/main/java/com/example/Server.java
+++ src/main/java/com/example/Server.java
@@ -6,43 +6,47 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
 
-public class Server {
-    ServerSocket server;    
+public class Server extends Thread {
     Socket connection;
     BufferedReader in;
     DataOutputStream out;
     Integer randomNumber;
     Integer attempts = 1;
 
-    public Server(int port) throws IOException{
-        server = new ServerSocket(port);
-        System.out.println("Server listening at port " + port);
-        connection = server.accept();
+    public Server(Socket newConnection) throws IOException {
+        // server = new ServerSocket(port);
+        // System.out.println("Server listening at port " + port);
+        this.connection = newConnection;
         in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         out = new DataOutputStream(connection.getOutputStream());
-        randomNumber = (int) (Math.random() * 100 + 1); 
-    }    
+        randomNumber = (int) (Math.random() * 100 + 1);
+    }
 
-    public void start() throws IOException {
-        String userInput = in.readLine();
-        Integer procUserInput = Integer.parseInt(userInput);
+    public void run() {
+        try {
 
-        while (procUserInput != randomNumber) {
-            System.out.println("User input: " + userInput);
-            if (procUserInput > randomNumber) {
-                out.writeBytes("1\n");
-            } else {
-                out.writeBytes("2\n");
+            String userInput = in.readLine();
+            Integer procUserInput = Integer.parseInt(userInput);
+
+            while (procUserInput != randomNumber) {
+                System.out.println("User input: " + userInput);
+                if (procUserInput > randomNumber) {
+                    out.writeBytes("1\n");
+                } else {
+                    out.writeBytes("2\n");
+                }
+
+                userInput = in.readLine();
+                procUserInput = Integer.parseInt(userInput);
+
+                attempts++;
             }
-            
-            userInput = in.readLine();
-            procUserInput = Integer.parseInt(userInput);
-
-            attempts++;
+            out.writeBytes("3\n");
+            out.writeBytes(attempts + "\n");
+            connection.close();
+        } catch (Exception e) {
+            System.err.println("FUCK");
+            System.exit(1);
         }
-        out.writeBytes("3\n");
-        out.writeBytes(attempts + "\n");
-        server.close();
-        connection.close();
     }
 }
